5c7b05c21efbfe637df83f06f9abba136284ce9f
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-04-08 16:52:05 +0000

    po konfiguracji i testach

--- ColCalc/src/pl/looksok/activity/ColCalcActivity.java
+++ ColCalc/src/pl/looksok/activity/ColCalcActivity.java
@@ -1,5 +1,6 @@
-package pl.looksok;
+package pl.looksok.activity;
 
+import pl.looksok.R;
 import android.app.Activity;
 import android.os.Bundle;
 
---
+++ ColCalcTest/src/pl/looksok/test/logic/CcLogicTest.java
@@ -0,0 +1,19 @@
+package pl.looksok.test.logic;
+
+import junit.framework.TestCase;
+
+public class CcLogicTest extends TestCase {
+
+	public CcLogicTest() {
+		super();
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void testConstructor(){
+		fail();
+	}
+
+}
