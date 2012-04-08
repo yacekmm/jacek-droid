a1aa5b5d34891265617f9b0cc9ae536c5c7e0347
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-04-08 20:33:09 +0000

    pierwsza wersja zwrotów pomiedzy dwoma osobami (dziala tez gdy obie sie dorzucaly)

--- ColCalc/src/pl/looksok/logic/CcLogic.java
+++ ColCalc/src/pl/looksok/logic/CcLogic.java
@@ -1,5 +1,60 @@
 package pl.looksok.logic;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+
+import pl.looksok.exception.BadPayException;
+import pl.looksok.exception.BadPeopleCountException;
+
 public class CcLogic {
+	private Hashtable<String, PeoplePays> calculationResult;
+	
+	public CcLogic(){
+		calculationResult = new Hashtable<String, PeoplePays>();
+	}
+
+	public double howMuchPerPerson(double totalPay, int peopleCount) {
+		if(totalPay < 0)
+			throw new BadPayException();
+		if(peopleCount < 0)
+			throw new BadPeopleCountException();
+		
+		return totalPay / peopleCount;
+	}
+
+	public Hashtable<String, PeoplePays> calculate(HashMap<String, Double> inputPays) {
+		Double totalPay = calculateTotalPayValue(inputPays);
+		int peopleCount = inputPays.size();
+		double howMuchPerPerson = howMuchPerPerson(totalPay, peopleCount);
+		
+		Collection<String> c = inputPays.keySet();
+		Iterator<String> itr = c.iterator();
+		while (itr.hasNext()){
+			String key = itr.next();
+			PeoplePays p = new PeoplePays(key, inputPays);
+			p.calculateToReturn(howMuchPerPerson);
+			calculationResult.put(p.getPersonName(), p);
+		}
+		
+		return calculationResult;
+	}
+
+	private Double calculateTotalPayValue(HashMap<String, Double> inputPays) {
+		Double totalPay = 0.0;
+		
+		Collection<Double> c = inputPays.values();
+		Iterator<Double> itr = c.iterator();
+
+		while (itr.hasNext()){
+			totalPay += itr.next();
+		}
+		return totalPay;
+	}
+
+	public double howMuchPersonAGivesBackToPersonB(String personA, String personB) {
+		return calculationResult.get(personA).howMuchShouldReturnTo(personB);
+	}
 
 }
---
+++ ColCalc/src/pl/looksok/logic/PeoplePays.java
@@ -0,0 +1,67 @@
+package pl.looksok.logic;
+
+import java.util.HashMap;
+
+
+public class PeoplePays {
+
+	private String personName;
+	private double howMuchIPaid;
+	private double toReturn;
+	private double totalRefundForThisPerson;
+	private HashMap<String, Double> otherPeoplePayments;
+	private double howMuchPerPerson;
+	
+	public PeoplePays(String _personName, HashMap<String, Double> inputPays) {
+		setPersonName(_personName);
+		setPayMadeByPerson(inputPays.get(getPersonName()));
+		
+		otherPeoplePayments = new HashMap<String, Double>();
+		otherPeoplePayments = inputPays;
+		//otherPeoplePayments.remove(_personName);
+	}
+
+	public String getPersonName() {
+		return personName;
+	}
+
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+
+	public double getPayMadeByPerson() {
+		return howMuchIPaid;
+	}
+
+	public void setPayMadeByPerson(Double payMadeByPerson) {
+		this.howMuchIPaid = payMadeByPerson;
+	}
+
+	public void calculateToReturn(double _howMuchPerPerson) {
+		howMuchPerPerson = _howMuchPerPerson;
+		toReturn = howMuchPerPerson - howMuchIPaid;
+		if(toReturn < 0.0)
+			toReturn = 0.0;
+		
+		totalRefundForThisPerson = howMuchIPaid - howMuchPerPerson;
+		if(totalRefundForThisPerson < 0.0)
+			totalRefundForThisPerson = 0.0;
+	}
+
+	public double getToReturn() {
+		return toReturn;
+	}
+
+	public double getTotalRefundForThisPerson() {
+		return totalRefundForThisPerson;
+	}
+	
+	public double howMuchShouldReturnTo(String personB){
+		double howMuchPersonBPaid = otherPeoplePayments.get(personB);
+		if(howMuchPersonBPaid > howMuchIPaid){
+			return howMuchPersonBPaid - howMuchPerPerson;
+		}else
+			return 0.0;
+	}
+
+}
