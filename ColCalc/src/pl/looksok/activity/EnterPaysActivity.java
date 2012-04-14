4eeaf3bb4d0ca5d99d4c90bbe7473892f6d82c13
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-04-14 13:09:59 +0000

    pierwsza wersja activity do dodawania nowych osób

--- ColCalc/src/pl/looksok/activity/EnterPaysActivity.java
+++ ColCalc/src/pl/looksok/activity/EnterPaysActivity.java
@@ -4,11 +4,11 @@ import pl.looksok.R;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class ColCalcActivity extends Activity {
+public class EnterPaysActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.enter_pays);
     }
 }
---
+++ ColCalcTest/src/pl/looksok/test/activity/EnterPaysActivityTest.java
@@ -0,0 +1,69 @@
+package pl.looksok.test.activity;
+
+import pl.looksok.activity.EnterPaysActivity;
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+import android.test.ViewAsserts;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class EnterPaysActivityTest extends
+		ActivityUnitTestCase<EnterPaysActivity> {
+	
+	private EnterPaysActivity mActivity;
+	private View origin;
+	
+	private TextView mTitileText;
+	private TextView mNewPersonNameText;
+	private TextView mNewPersonNameEdit;
+	private TextView mPayText;
+	private TextView mPayEdit;
+	private Button mAddPErsonButton;
+
+	public EnterPaysActivityTest() {
+		super(EnterPaysActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		startActivity(new Intent(), null, null);
+		mActivity = getActivity();
+		origin = mActivity.getWindow().getDecorView();
+		
+		mTitileText = (TextView)mActivity.findViewById(pl.looksok.R.id.EnterPays_TextView_Title);
+		mNewPersonNameText = (TextView)mActivity.findViewById(pl.looksok.R.id.EnterPays_TextView_Name);
+		mNewPersonNameEdit = (TextView)mActivity.findViewById(pl.looksok.R.id.EnterPays_EditText_Name);
+		mPayText = (TextView)mActivity.findViewById(pl.looksok.R.id.EnterPays_TextView_Pay);
+		mPayEdit = (TextView)mActivity.findViewById(pl.looksok.R.id.EnterPays_EditText_Pay);
+		mAddPErsonButton = (Button)mActivity.findViewById(pl.looksok.R.id.EnterPays_Button_AddPerson);
+	}
+	
+	public void testInitialization(){
+		assertNotNull(mActivity);
+		assertNotNull(mTitileText);
+		assertNotNull(mNewPersonNameText);
+		assertNotNull(mNewPersonNameEdit);
+		assertNotNull(mPayText);
+		assertNotNull(mPayEdit);
+		assertNotNull(mAddPErsonButton);
+	}
+	
+	public void testViewsOnScreen(){
+		ViewAsserts.assertOnScreen(origin, mTitileText);
+		ViewAsserts.assertOnScreen(origin, mNewPersonNameText);
+		ViewAsserts.assertOnScreen(origin, mNewPersonNameEdit);
+		ViewAsserts.assertOnScreen(origin, mPayText);
+		ViewAsserts.assertOnScreen(origin, mPayEdit);
+		ViewAsserts.assertOnScreen(origin, mAddPErsonButton);
+	}
+	
+	public void testViewsAlignment(){
+		ViewAsserts.assertHorizontalCenterAligned(origin, mTitileText);
+		
+		ViewAsserts.assertLeftAligned(origin, mTitileText);
+		ViewAsserts.assertRightAligned(origin, mTitileText);
+		ViewAsserts.assertTopAligned(origin, mTitileText);
+	}
+}
