91787748106f229102fe2abf3a563bf6722f080e
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-04-19 19:43:38 +0000

    zapis i odczyt jednego rozliczenia

--- ColCalc/src/pl/looksok/activity/CalculationActivity.java
+++ ColCalc/src/pl/looksok/activity/CalculationActivity.java
@@ -1,28 +1,66 @@
 package pl.looksok.activity;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 import pl.looksok.R;
 import pl.looksok.logic.CcLogic;
 import pl.looksok.utils.Constants;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class CalculationActivity extends Activity {
 	private CcLogic calc = null;
+	private Button mSaveButton;
 	
     @Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculation);
         
-        Bundle extras = getIntent().getExtras();
+        readInputBundle();
+        
+        mSaveButton = (Button)findViewById(R.id.calculation_button_save);
+        mSaveButton.setOnClickListener(saveButtonClickListener);
+    }
+
+	OnClickListener saveButtonClickListener = new OnClickListener() {
+        public void onClick(View v) {
+    		String filename = "installedapplist.txt";
+    	    	//Stores the object to internal storage at the filename above
+
+    	    	FileOutputStream fos = null;
+    		ObjectOutputStream out = null;
+    		try {
+    			fos = openFileOutput(filename, Context.MODE_PRIVATE);
+    			out = new ObjectOutputStream(fos);
+    			out.writeObject(calc);
+    			out.close();
+    			fos.close();
+    			
+    			Log.v("saveList", "Successful Save");
+    		} catch (IOException ex) {
+    			ex.printStackTrace();
+    		}
+        }
+    };
+    
+	private void readInputBundle() {
+		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
 			calc = (CcLogic)extras.getSerializable(Constants.BUNDLE_CALCULATION_OBJECT);
 			TextView view = (TextView) findViewById(R.id.Calculation_text_debug);
 			view.setText(calc.toString());
 		}
-    }
+	}
 
 	@Override
 	public void onBackPressed() {
--- ColCalc/src/pl/looksok/activity/WelcomeActivity.java
+++ ColCalc/src/pl/looksok/activity/WelcomeActivity.java
@@ -1,9 +1,18 @@
 package pl.looksok.activity;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.StreamCorruptedException;
+
 import pl.looksok.R;
+import pl.looksok.logic.CcLogic;
+import pl.looksok.utils.Constants;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -12,6 +21,8 @@ public class WelcomeActivity extends Activity {
 
 	private Button mNewButton;
 
+	private Button mLoadButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,6 +30,9 @@ public class WelcomeActivity extends Activity {
 		
 		mNewButton = (Button)findViewById(R.id.welcome_Button_newCalculation);
 		mNewButton.setOnClickListener(newCalculationButtonClickListener);
+		
+		mLoadButton = (Button)findViewById(R.id.welcome_Button_loadCalculation);
+		mLoadButton.setOnClickListener(loadCalculationButtonClickListener);
 	}
 	
 	OnClickListener newCalculationButtonClickListener = new OnClickListener() {
@@ -28,6 +42,38 @@ public class WelcomeActivity extends Activity {
         	finish();
         }
     };
-
-	
+    
+	OnClickListener loadCalculationButtonClickListener = new OnClickListener() {
+        public void onClick(View v) {
+        	String filename = "installedapplist.txt";
+        	CcLogic calc = null;
+        	
+        	FileInputStream fis = null;
+        	ObjectInputStream in = null;
+        	try {
+        		fis = openFileInput(filename);
+        		in = new ObjectInputStream(fis);
+        		calc = (CcLogic) in.readObject();
+        		in.close();
+        		fis.close();
+        		
+            	Intent intent = new Intent(getApplicationContext(), EnterPaysActivity.class) ;
+            	intent.putExtra(Constants.BUNDLE_CALCULATION_OBJECT, calc);
+            	startActivity(intent);
+            	finish();
+        		
+        	}catch (ClassNotFoundException e) {
+        		e.printStackTrace();
+        		
+        	} catch (FileNotFoundException e) {
+        		e.printStackTrace();
+        	} catch (StreamCorruptedException e) {
+        		
+        	} catch (IOException e) {
+        		e.printStackTrace();
+        	}
+        	
+        	Log.v("opened Calc", "success");
+        }
+    };
 }
