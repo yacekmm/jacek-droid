eb7c72a96a4f11eaafce5c3bab9408c0ea7e8748
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-04-21 12:49:11 +0000

    wzniesienie logiki persistence

--- trunk/ColCalc/src/pl/looksok/activity/WelcomeActivity.java
+++ trunk/ColCalc/src/pl/looksok/activity/WelcomeActivity.java
@@ -1,18 +1,12 @@
 package pl.looksok.activity;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.StreamCorruptedException;
-
 import pl.looksok.R;
 import pl.looksok.logic.CcLogic;
+import pl.looksok.utils.CalcPersistence;
 import pl.looksok.utils.Constants;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -45,35 +39,12 @@ public class WelcomeActivity extends Activity {
     
 	OnClickListener loadCalculationButtonClickListener = new OnClickListener() {
         public void onClick(View v) {
-        	String filename = "installedapplist.txt";
-        	CcLogic calc = null;
+        	CcLogic calc = CalcPersistence.readStoredCalculation(getApplicationContext(), "installedapplist.txt");
         	
-        	FileInputStream fis = null;
-        	ObjectInputStream in = null;
-        	try {
-        		fis = openFileInput(filename);
-        		in = new ObjectInputStream(fis);
-        		calc = (CcLogic) in.readObject();
-        		in.close();
-        		fis.close();
-        		
-            	Intent intent = new Intent(getApplicationContext(), EnterPaysActivity.class) ;
-            	intent.putExtra(Constants.BUNDLE_CALCULATION_OBJECT, calc);
-            	startActivity(intent);
-            	finish();
-        		
-        	}catch (ClassNotFoundException e) {
-        		e.printStackTrace();
-        		
-        	} catch (FileNotFoundException e) {
-        		e.printStackTrace();
-        	} catch (StreamCorruptedException e) {
-        		
-        	} catch (IOException e) {
-        		e.printStackTrace();
-        	}
-        	
-        	Log.v("opened Calc", "success");
+        	Intent intent = new Intent(getApplicationContext(), EnterPaysActivity.class) ;
+        	intent.putExtra(Constants.BUNDLE_CALCULATION_OBJECT, calc);
+        	startActivity(intent);
+        	finish();
         }
     };
 }
