edb164ea7b6d43a33ee108985d3550584a1a57bf
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-06-02 12:17:59 +0000

    pobieranie listy stacji

---
+++ trunk/TrainTracker/src/pl/looksok/traintracker/utils/Constants.java
@@ -0,0 +1,7 @@
+package pl.looksok.traintracker.utils;
+
+public class Constants {
+	public static final String WS_CP_STATION_MATCH_LIST = "findStation";
+	
+	public static final String WS_URL_IC_STATION_LIST = "http://infopasazer.intercity.pl/index_set.php";
+}
