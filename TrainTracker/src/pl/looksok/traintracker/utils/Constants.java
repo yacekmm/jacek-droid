fe32425baec26d4441289bb1580703fe9c6d605d
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-06-04 16:33:21 +0000

    PRzed dodaniem grafiki

--- TrainTracker/src/pl/looksok/traintracker/utils/Constants.java
+++
@@ -1,7 +0,0 @@
-package pl.looksok.traintracker.utils;
-
-public class Constants {
-	public static final String WS_CP_STATION_MATCH_LIST = "findStation";
-	
-	public static final String WS_URL_IC_STATION_LIST = "http://infopasazer.intercity.pl/index_set.php";
-}
