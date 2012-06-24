5adff50fec77c68e7e9fd479c142bf3751e229eb
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-06-24 15:20:33 +0000

    pierwsza wersja emaili

--- ColCalc/src/pl/looksok/activity/addperson/AddPersonUtils.java
+++ ColCalc/src/pl/looksok/activity/addperson/AddPersonUtils.java
@@ -1,13 +1,27 @@
 package pl.looksok.activity.addperson;
 
-import android.content.Context;
+import java.util.HashSet;
+
+import android.database.Cursor;
+import android.provider.ContactsContract;
 
 public class AddPersonUtils {
 
-	@SuppressWarnings("unused")
-	private Context context;
-	
-	public AddPersonUtils(Context applicationContext) {
-		this.context = applicationContext;
+	public HashSet<String> getPersonEmailsSet(String id, AddNewPerson addNewPerson){
+        HashSet<String> emails = new HashSet<String>(); 
+		Cursor emailCur = addNewPerson.managedQuery( 
+  		ContactsContract.CommonDataKinds.Email.CONTENT_URI, 
+			null,
+			ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?", 
+			new String[]{id}, null); 
+	      	
+		while (emailCur.moveToNext()) { 
+      		String email = emailCur.getString(
+                      emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+      		emails.add(email);
+      	}
+		
+		return emails;
+      	
 	}
 }
