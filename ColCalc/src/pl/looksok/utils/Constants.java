70b38732cd4a2fae8810fa1a4f6f87e1272144c0
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-06-30 09:55:04 +0000

    21 DONE

--- ColCalc/src/pl/looksok/utils/Constants.java
+++ ColCalc/src/pl/looksok/utils/Constants.java
@@ -2,8 +2,10 @@ package pl.looksok.utils;
 
 public class Constants {
 	public static final String BUNDLE_CALCULATION_OBJECT = "CalculationObject";
-	public static String BUNDLE_INPUT_DATA_OBJECT = "InputDataObject";
+	public static final String BUNDLE_INPUT_DATA_OBJECT = "InputDataObject";
+	public static final String BUNDLE_PERSON_TO_EDIT = "personToEdit";
 	
 	public static final String SHARED_PREFS_NAME = "PayCalc";
-	public static final String BUNDLE_PERSON_TO_EDIT = "personToEdit";
+	
+	public static final String APPLICATION_WEBSITE_URL = "http://looksok.wordpress.com/";
 }
