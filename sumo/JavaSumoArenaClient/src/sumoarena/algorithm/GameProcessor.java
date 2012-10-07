f9c79d109add7348313f10ae7c3f6e6456f7ee88
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-10-07 19:13:11 +0000

    wdrozzone tw. cosinusow

--- sumo/JavaSumoArenaClient/src/sumoarena/algorithm/GameProcessor.java
+++ sumo/JavaSumoArenaClient/src/sumoarena/algorithm/GameProcessor.java
@@ -10,6 +10,7 @@ import valueobjects.Sphere;
 
 public class GameProcessor {
 	
+	private static final int MAX_DESIRED_LENGTH = 100;
 	private double rescueThreshold = 0.65;
 	private RoundStartInfo roundInfo;
 	
@@ -18,11 +19,10 @@ public class GameProcessor {
 		Sphere me = findMe(playingInfo.getSpheres());
 		Sphere enemy = findEnemy(playingInfo.getSpheres());
 		
-		Point pointToFollow = new Point(50,50);
+		Point pointToFollow = new Point(0,0);
 		
 		if(enemy!=null){
-			pointToFollow.x = enemy.x;
-			pointToFollow.y = enemy.y;
+			pointToFollow = new Point(enemy.x, enemy.y);
 			System.out.println("Chasing enemy!: " + enemy.index + ", " + pointToFollow + ", distance: " + Point2D.distance(pointToFollow.x, pointToFollow.y, me.x, me.y) + ", me: " + new Point(me.x, me.y));
 		}
 
@@ -30,9 +30,15 @@ public class GameProcessor {
 		
 		if(iAmTooCloseToEdge(playingInfo, distanceFromMiddle) && iAmApproachingToEdge(me)){
 			System.out.println("----Trying to backout! Arena size: " + playingInfo.getArenaRadius());
-			return reduceFallOffRisk(me);
-		}else
-			return attackPoint(pointToFollow, me);
+			return reduceFallOffRisk(me, playingInfo);
+		}else{
+			AccelerationVector accVec = attackPoint(pointToFollow, me, playingInfo);
+			if(iAmApproachingToEdge(me) && iWillFallOffAfterThisStep(me, accVec, playingInfo) ){
+				System.out.println("\n\n!!! You are going to fall off!!! Backing off to (0,0)");
+				accVec = attackPoint(new Point(0,0), me, playingInfo);
+			}
+			return accVec;
+		}
 	}
 
 	private boolean iAmApproachingToEdge(Sphere me) {
@@ -51,40 +57,62 @@ public class GameProcessor {
 		return distanceFromMiddle/playingInfo.getArenaRadius() > rescueThreshold;
 	}
 	
-	private AccelerationVector reduceFallOffRisk(Sphere me) {
-		return attackPoint(new Point(0, 0), me);
+	private AccelerationVector reduceFallOffRisk(Sphere me, PlayingInfo playingInfo) {
+		return attackPoint(new Point(0, 0), me, playingInfo);
 	}
 
-	private AccelerationVector attackPoint(Point pointToStop, Sphere me){
-		int accX = 0;
-		int accY = 0;
+	private AccelerationVector attackPoint(Point pointToAttack, Sphere me, PlayingInfo playingInfo){
 		
-		System.out.println("Going to point:\t" + pointToStop);
+		System.out.println("Going to point:\t" + pointToAttack);
 		
-		int desiredVX = pointToStop.x - me.x;
-		int desiredVY = pointToStop.y - me.y;
-		AccelerationVector desiredAcc = new AccelerationVector(desiredVX, desiredVY);
-		System.out.println("Desired vector:\t" + desiredAcc.getdVx() + ", " + desiredAcc.getdVy() + "\tlength: " + MathHelper.getVectorLength(desiredAcc));
-//		desiredAcc = MathHelper.scaleVectorToLength(desiredAcc, roundInfo.maxSpeedVariation);
-//		System.out.println("Scaled desired:\t" + desiredAcc.getdVx() + ", " + desiredAcc.getdVy() + "\tlength: " + MathHelper.getVectorLength(desiredAcc));
-		
+		AccelerationVector desiredAcc = new AccelerationVector(pointToAttack.x - me.x, pointToAttack.y - me.y);
+		double desiredLength = MathHelper.getVectorLength(desiredAcc);
+		System.out.println("Desired vector:\t" + desiredAcc.getdVx() + ", " + desiredAcc.getdVy() + "\tlength: " + desiredLength);
+
 		//add desired - cur
-		accX =  desiredAcc.getdVx() - me.vx;
-		accY =  desiredAcc.getdVy() - me.vy;
-		System.out.println("calc X:Y:\t" + accX + ":" + accY);
+		AccelerationVector accVec = MathHelper.sumVectors(desiredAcc, new AccelerationVector(-me.vx, -me.vy));
+		System.out.println("calc X:Y:\t" + accVec.getdVx() + ":" + accVec.getdVy());
 		
-		return normalize(new AccelerationVector(accX, accY), new AccelerationVector(desiredVX, desiredVY));
+		accVec = normalize(accVec, desiredAcc, me);
+		
+		return accVec;
 	}
 
+	private AccelerationVector reduceDesiredAcc(Point pointToAttack,
+			AccelerationVector desiredAcc, double desiredLength) {
+		if(desiredLength > MAX_DESIRED_LENGTH && pointToAttack.x != 0 && pointToAttack.y != 0){
+			System.out.println("@@@ reducing desired Length from: " + desiredLength);
+			desiredAcc = MathHelper.scaleVectorToLength(desiredAcc, MAX_DESIRED_LENGTH);
+		}
+		return desiredAcc;
+	}
 
+	private boolean iWillFallOffAfterThisStep(Sphere me, AccelerationVector accVec, PlayingInfo playingInfo) {
+		AccelerationVector resultVec = MathHelper.sumVectors(accVec, new AccelerationVector(me.vx, me.vy));
+		Point resultPos = new Point(me.x + resultVec.getdVx(), me.y + resultVec.getdVy());
+		double resultDistanceFromMiddle = resultPos.distance(new Point(0,0));
+		System.out.println("CurPos:\t" + new Point(me.x,me.y) + "\tResultPos:\t" + resultPos + "\tresultDistance:\t" + resultDistanceFromMiddle + "\tArenaRadius:\t" + playingInfo.getArenaRadius());
+		return resultDistanceFromMiddle > playingInfo.getArenaRadius();
+	}
 
-	private AccelerationVector normalize(AccelerationVector accVec, AccelerationVector desiredAccVec) {
-		double ratio = 1;
+	private AccelerationVector normalize(AccelerationVector accVec, AccelerationVector desiredAccVec, Sphere me) {
 
 		if(MathHelper.getVectorLength(accVec) > roundInfo.maxSpeedVariation ){
 			accVec = MathHelper.scaleVectorToLength(accVec, roundInfo.maxSpeedVariation);
+			double accVecLength = MathHelper.getVectorLength(accVec);
+			
+			AccelerationVector curVec = new AccelerationVector(me.vx, me.vy);
+			double curVecLength = MathHelper.getVectorLength(curVec);
+
+			//cosine theorem
+			double cosAlpha = MathHelper.cosAngleBetweenVectors(accVec, desiredAccVec);
+			double aFactor = 1;
+			double bFactor = -1 * 2 * cosAlpha * curVecLength;
+			double cFactor = curVecLength * curVecLength - accVecLength * accVecLength;
+			System.out.println("\n\n\n");
+			MathHelper.quadraticEquationSolver(aFactor, bFactor, cFactor);
     	}
-		System.out.println("limited X:Y:\t" + accVec.getdVx() + ":" + accVec.getdVy() + "\t ratio: " + ratio + "\tmaxSpeedVar: " + roundInfo.maxSpeedVariation);
+		System.out.println("limited X:Y:\t" + accVec.getdVx() + ":" + accVec.getdVy() + "\tmaxSpeedVar: " + roundInfo.maxSpeedVariation);
 		return accVec;
 	}
 
