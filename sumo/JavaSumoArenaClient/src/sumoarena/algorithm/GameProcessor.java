e007bf0beca3e23928c4630aa143388c4baddf91
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-10-08 04:58:08 +0000

    train refactor

--- sumo/JavaSumoArenaClient/src/sumoarena/algorithm/GameProcessor.java
+++ sumo/JavaSumoArenaClient/src/sumoarena/algorithm/GameProcessor.java
@@ -1,48 +1,54 @@
 package sumoarena.algorithm;
 
 import java.awt.Point;
-import java.awt.geom.Point2D;
 import java.math.RoundingMode;
-
-import com.google.common.math.DoubleMath;
 
 import valueobjects.AccelerationVector;
 import valueobjects.PlayingInfo;
 import valueobjects.RoundStartInfo;
 import valueobjects.Sphere;
 
+import com.google.common.math.DoubleMath;
+
 public class GameProcessor {
 
 	private static final double RESCUE_THRESHOLD = 0.60;
 	private static final int MAX_NORMALIZATION_TRIES = 10;
 	private static final int MAX_DESIRED_SPEED = 90;
+	private static final Point ARENA_CENTER = new Point(0,0);
 	
 	private RoundStartInfo roundInfo;
 	private PlayingInfo playingInfo;
 	private int normalizationCounter;
+	private Sphere me;
+	Point curPos;
+	AccelerationVector curVec;
 	
 	private void resetVariables(PlayingInfo _playingInfo) {
 		this.playingInfo = _playingInfo;
 		normalizationCounter = 0;
+		me = findMe(playingInfo.getSpheres());
+		curPos = new Point(me.x, me.y);
+		curVec = new AccelerationVector(me.vx, me.vy);
 	}
 
 	public AccelerationVector getAccVector(PlayingInfo _playingInfo) {
-		resetVariables(_playingInfo);
 		
 		System.out.println("");
-		Sphere me = findMe(playingInfo.getSpheres());
+		resetVariables(_playingInfo);
+
 		Sphere enemy = findEnemy(playingInfo.getSpheres());
 
-		Point pointToFollow = new Point(0,0);
+		Point pointToFollow = ARENA_CENTER;
 
 		if(enemy!=null){
 			pointToFollow = new Point(enemy.x, enemy.y);
-			System.out.println("Chasing enemy!: " + enemy.index + ", " + pointToFollow + ", distance: " + Point2D.distance(pointToFollow.x, pointToFollow.y, me.x, me.y) + ", me: " + new Point(me.x, me.y));
+			System.out.println("Chasing enemy!: " + enemy.index + ", " + pointToFollow + ", distance: " +  curPos.distance(pointToFollow) + ", me: " + curPos);
 		}
 
-		double distanceFromMiddle = Point.distance(me.x, me.y, 0, 0);
+		double distanceFromMiddle = curPos.distance(ARENA_CENTER);
 
-		if(iAmTooCloseToEdge(playingInfo, distanceFromMiddle) && iAmApproachingToEdge(me)){
+		if(iAmTooCloseToEdge(distanceFromMiddle) && iAmApproachingToEdge(me)){
 			return reduceFallOffRisk(me, playingInfo);
 		}else{
 			return attackPoint(pointToFollow, me, playingInfo, MAX_DESIRED_SPEED);
@@ -71,17 +77,13 @@ public class GameProcessor {
 
 		if(MathHelper.getVectorLength(accVec) > roundInfo.maxSpeedVariation ){
 			accVec = MathHelper.scaleVectorToLength(accVec, roundInfo.maxSpeedVariation);
-			double accVecLength = MathHelper.getVectorLength(accVec);
 
-			AccelerationVector curVec = new AccelerationVector(me.vx, me.vy);
-			double curVecLength = MathHelper.getVectorLength(curVec);
-
-			double targetDesVecLenth = getTargetDesVecLength(desiredAccVec, accVecLength, curVec, curVecLength);
+			double targetDesVecLenth = getTargetDesVecLength(desiredAccVec, accVec);
 			if(targetDesVecLenth > 0 && normalizationCounter < MAX_NORMALIZATION_TRIES){
 				normalizationCounter++;
-				int targetDesVecLenth2 = DoubleMath.roundToInt(targetDesVecLenth, RoundingMode.DOWN) - 1;
-				System.out.println("***scaling desiredVector to length:\t" + targetDesVecLenth2);
-				accVec = attackPoint(pointToAttack, me, playingInfo, targetDesVecLenth2);
+				int targetDesVecLenthInt = DoubleMath.roundToInt(targetDesVecLenth, RoundingMode.DOWN) - 1;
+				System.out.println("***scaling desiredVector to length:\t" + targetDesVecLenthInt);
+				accVec = attackPoint(pointToAttack, me, playingInfo, targetDesVecLenthInt);
 			}
 		}
 		System.out.println("limited X:Y:\t" + accVec.getdVx() + ":" + accVec.getdVy() + "\tmaxSpeedVar: " + roundInfo.maxSpeedVariation);
@@ -89,32 +91,31 @@ public class GameProcessor {
 	}
 
 	private double getTargetDesVecLength(AccelerationVector desiredAccVec,
-			double accVecLength, AccelerationVector curVec, double curVecLength) {
+			AccelerationVector accVec) {
+		
+		double curVecLength = MathHelper.getVectorLength(curVec);
+		double accVecLength = MathHelper.getVectorLength(accVec);
+		
 		//cosine theorem
 		double cosAlpha = MathHelper.cosAngleBetweenVectors(curVec, desiredAccVec);
 		double aFactor = 1;
 		double bFactor = -1 * 2 * cosAlpha * curVecLength;
 		double cFactor = curVecLength * curVecLength - accVecLength * accVecLength;
-		System.out.println("\n\n\n");
 		return MathHelper.quadraticEquationSolver(aFactor, bFactor, cFactor);
 	}
 
 	private boolean iAmApproachingToEdge(Sphere me) {
-		Point curPos = new Point(me.x, me.y);
 		Point futurePos = new Point(me.x + me.vx, me.y + me.vy);
-		Point center = new Point(0,0);
-
-		return futurePos.distance(center) > curPos.distance(center);
+		return futurePos.distance(ARENA_CENTER) > curPos.distance(ARENA_CENTER);
 	}
 
-	private boolean iAmTooCloseToEdge(PlayingInfo playingInfo,
-			double distanceFromMiddle) {
+	private boolean iAmTooCloseToEdge(double distanceFromMiddle) {
 		return distanceFromMiddle/playingInfo.getArenaRadius() > RESCUE_THRESHOLD;
 	}
 
 	private AccelerationVector reduceFallOffRisk(Sphere me, PlayingInfo playingInfo) {
 		System.out.println("----Trying to backout! Arena size: " + playingInfo.getArenaRadius());
-		return attackPoint(new Point(0, 0), me, playingInfo, MAX_DESIRED_SPEED*2);
+		return attackPoint(ARENA_CENTER, me, playingInfo, MAX_DESIRED_SPEED);
 	}
 
 	private Sphere findEnemy(Sphere[] spheres) {
@@ -136,5 +137,4 @@ public class GameProcessor {
 	public void setRoundInfo(RoundStartInfo roundInfo) {
 		this.roundInfo = roundInfo;
 	}
-
 }
