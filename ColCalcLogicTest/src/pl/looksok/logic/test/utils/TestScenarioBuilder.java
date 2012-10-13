3324fccad360a8ce2005a841e295465154a69c74
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-10-13 15:01:37 +0000

    HEavy revactor - wyniesienie logiki kalkulatora do zewnetrznego projektu JAva ColCalcLogic, niezaleznego od android i iuniezaleznienie testow od androida

---
+++ ColCalcLogicTest/src/pl/looksok/logic/test/utils/TestScenarioBuilder.java
@@ -0,0 +1,63 @@
+package pl.looksok.logic.test.utils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import pl.looksok.logic.PersonData;
+
+public class TestScenarioBuilder {
+	public static List<PersonData> buildTestCaseOnePerson(double paymentA) {
+		List<PersonData> inputPaysList = new  ArrayList<PersonData>();
+		inputPaysList.add(new PersonData(Constants.personAName, paymentA, new HashSet<String>()));
+		return inputPaysList;
+	}
+
+	public static List<PersonData> buildTestCaseTwoPeople(double paymentA, double paymentB) {
+		List<PersonData> inputPaysList = new  ArrayList<PersonData>();
+		inputPaysList = buildTestCaseOnePerson(paymentA);
+		inputPaysList.add(new PersonData(Constants.personBName, paymentB, new HashSet<String>()));
+		return inputPaysList;
+	}
+	
+	public static List<PersonData> buildTestCaseTwoPeopleVariousPays(double paymentA, double howMuchAShouldPay,
+			double paymentB, double howMuchBShouldPay) {
+		List<PersonData> inputPaysList = new  ArrayList<PersonData>();
+		inputPaysList.add(new PersonData(Constants.personAName, paymentA, howMuchAShouldPay, new HashSet<String>()));
+		inputPaysList.add(new PersonData(Constants.personBName, paymentB, howMuchBShouldPay, new HashSet<String>()));
+		return inputPaysList;
+	}
+
+	public static List<PersonData> buildTestCaseThreePeople(double paymentA, double paymentB, double paymentC) {
+		List<PersonData> inputPaysList = new  ArrayList<PersonData>();
+		inputPaysList = buildTestCaseTwoPeople(paymentA, paymentB);
+		inputPaysList.add(new PersonData(Constants.personCName, paymentC, new HashSet<String>()));
+		return inputPaysList;
+	}
+	
+	public static List<PersonData> buildTestCaseThreePeople(String personAName, double paymentA, 
+			String personBName, double paymentB, 
+			String personCName, double paymentC) {
+		List<PersonData> inputPaysList = new  ArrayList<PersonData>();
+		inputPaysList.add(new PersonData(personAName, paymentA, new HashSet<String>()));
+		inputPaysList.add(new PersonData(personBName, paymentB, new HashSet<String>()));
+		inputPaysList.add(new PersonData(personCName, paymentC, new HashSet<String>()));
+		return inputPaysList;
+	}
+
+	public static List<PersonData> buildTestCaseFourPeople(double paymentA, double paymentB, double paymentC, double paymentD) {
+		List<PersonData> inputPaysList = new  ArrayList<PersonData>();
+		inputPaysList = buildTestCaseThreePeople(paymentA, paymentB, paymentC);
+		inputPaysList.add(new PersonData(Constants.personDName, paymentD, new HashSet<String>()));
+		return inputPaysList;
+	}
+	
+	public static List<PersonData> buildTestCaseSixPeople(double paymentA, double paymentB, double paymentC, 
+			double paymentD, double paymentE, double paymentF) {
+		List<PersonData> inputPaysList = new  ArrayList<PersonData>();
+		inputPaysList = buildTestCaseFourPeople(paymentA, paymentB, paymentC, paymentD);
+		inputPaysList.add(new PersonData(Constants.personEName, paymentE, new HashSet<String>()));
+		inputPaysList.add(new PersonData(Constants.personFName, paymentF, new HashSet<String>()));
+		return inputPaysList;
+	}
+}
