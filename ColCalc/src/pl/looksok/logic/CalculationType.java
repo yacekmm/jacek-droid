f9a6ed14b31290b63c6fd40e192245b134523bcb
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-10-13 15:02:20 +0000

    HEavy revactor - wyniesienie logiki kalkulatora do zewnetrznego projektu JAva ColCalcLogic, niezaleznego od android i iuniezaleznienie testow od androida

--- ColCalc/src/pl/looksok/logic/CalculationType.java
+++
@@ -1,9 +0,0 @@
-package pl.looksok.logic;
-
-public enum CalculationType {
-	POTLUCK_PARTY,
-	POTLUCK_PARTY_WITH_GIFT,
-	RESTAURANT,
-	EQUAL_PAYMENTS, 
-	DEFAULT
-}
--- ColCalc/src/pl/looksok/logic/exceptions/BadPeopleCountException.java
+++
@@ -1,6 +0,0 @@
-package pl.looksok.logic.exceptions;
-
-public class BadPeopleCountException extends RuntimeException {
-	private static final long serialVersionUID = 4747385414525791231L;
-
-}
