bc4f950df71d903595acdce96a12ee29347ddd60
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-10-14 11:30:20 +0000

    Scroller z dodawaniem liczby osob w multiperson

--- trunk/ColCalc/src/pl/looksok/activity/addperson/AddNewPersonMulti.java
+++ trunk/ColCalc/src/pl/looksok/activity/addperson/AddNewPersonMulti.java
@@ -5,12 +5,18 @@ import java.util.HashSet;
 import pl.looksok.R;
 import pl.looksok.logic.PersonData;
 import pl.looksok.logic.exceptions.BadInputDataException;
+import pl.looksok.utils.Constants;
 import pl.looksok.utils.FormatterHelper;
-import android.widget.EditText;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class AddNewPersonMulti extends AddNewPersonBase {
 
-	private EditText mHowManyPersons;
+	private LinearLayout mHowManyPersonsScroller;
+	private Button mHowManyPersonsSelectedBtn;
 
 	@Override
 	protected int getAddPersonContentView() {
@@ -21,22 +27,46 @@ public class AddNewPersonMulti extends AddNewPersonBase {
 	protected int getPayInputResId() {
 		return R.id.enterPaysMulti_EditText_Pay;
 	}
-	
+
 	@Override
 	protected void initActivityViews() {
 		super.initActivityViews();
-		mHowManyPersons = (EditText)findViewById(R.id.enterPaysMulti_EditText_peopleCount);
-		mHowManyPersons.setOnFocusChangeListener(editTextFocusListener);
 		
+		mHowManyPersonsScroller = (LinearLayout)findViewById(R.id.enterPaysMulti_peopleCount_content);
+		for(int i=Constants.MULTI_PERSON_MIN_COUNT; i<Constants.MULTI_PERSON_MAX_COUNT; i++){
+			Button b = new Button(getApplicationContext());
+			b.setLayoutParams(
+					new LayoutParams(
+							getResources().getDimensionPixelSize(R.dimen.height_button),
+							LayoutParams.FILL_PARENT));
+			b.setBackgroundResource(R.drawable.button_background_gray_active);
+			if(i==Constants.MULTI_PERSON_DEFAULT_COUNT){
+				b.setBackgroundColor(getResources().getColor(R.color.green_kotak));
+				mHowManyPersonsSelectedBtn = b;
+			}
+			b.setOnClickListener(setPeopleCountOnClickListener);
+			mHowManyPersonsScroller.addView(b);
+			b.setText("" + i);
+		}
 	}
+
+	private OnClickListener setPeopleCountOnClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			mHowManyPersonsSelectedBtn.setBackgroundResource(R.drawable.button_background_gray_active);
+			mHowManyPersonsSelectedBtn = (Button)v;
+			mHowManyPersonsSelectedBtn.setBackgroundColor(getResources().getColor(R.color.green_kotak));
+		}
+	};
+
 
 	@Override
 	protected HashSet<PersonData> getNewInputDataToAdd() throws BadInputDataException {
 		HashSet<PersonData> personDataSet = new HashSet<PersonData>();
 		double payDouble = FormatterHelper.readDoubleFromEditText(mNewPersonPayInput);
-		double howManyPersons = FormatterHelper.readDoubleFromEditText(mHowManyPersons);
+		double howManyPersons = Integer.parseInt(mHowManyPersonsSelectedBtn.getText().toString());
 		String namePrefix = getString(R.string.enterPaysMulti_personPrefix);
-		
+
 		int nameOffset = 0;
 		for(int i = 1; i<=howManyPersons; i++){
 			String name = namePrefix + " " + (i + nameOffset);
@@ -44,7 +74,7 @@ public class AddNewPersonMulti extends AddNewPersonBase {
 				nameOffset++;
 				name = namePrefix + " " + (i + nameOffset);
 			}
-			
+
 			personDataSet.add(new PersonData(name, payDouble, emails));
 		}
 		return personDataSet;
@@ -53,7 +83,7 @@ public class AddNewPersonMulti extends AddNewPersonBase {
 	private boolean inputListContainsPerson(String personName){
 		for (PersonData pd : inputPaysList) {
 			if(pd.getName().equals(personName))
-					return true;
+				return true;
 		}
 		return false;
 	}
