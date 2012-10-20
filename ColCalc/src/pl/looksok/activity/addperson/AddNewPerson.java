4007bb4d39d77d7a856c2603415802a351cbe691
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-10-20 14:26:05 +0000

    usuniecie z widoku i activity pol nierownych skladek (howMuchShouldPay)

--- ColCalc/src/pl/looksok/activity/addperson/AddNewPerson.java
+++ ColCalc/src/pl/looksok/activity/addperson/AddNewPerson.java
@@ -16,9 +16,6 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.CheckBox;
@@ -26,13 +23,9 @@ import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 public class AddNewPerson extends AddNewPersonBase {
-	CheckBox mEqualPaymentsBox;
 	EditText mNewPersonNameInput;
-	EditText mNewPersonShouldPayInput;
-	TextView mNewPersonShouldPayText;
 	CheckBox mReceivesGiftCheckBox;
 	CheckBox mBuysGiftCheckBox;
 	EditText mGiftValueInput;
@@ -53,34 +46,23 @@ public class AddNewPerson extends AddNewPersonBase {
 	protected void initActivityViews() {
 		super.initActivityViews();
 		((ImageButton)findViewById(R.id.EnterPays_button_getPersonFromContacts)).setOnClickListener(getContactClickListener);
-		mEqualPaymentsBox = (CheckBox) findViewById(R.id.EnterPays_CheckBox_EverybodyPaysEqually);
-		mEqualPaymentsBox.setOnCheckedChangeListener(equalPaysChangeListener);
-		mEqualPaymentsBox.setChecked(calc.isEqualPayments());
 		mNewPersonNameInput = (EditText)findViewById(R.id.EnterPays_EditText_Name);
-		mNewPersonShouldPayText = (TextView)findViewById(R.id.EnterPays_TextView_ShouldPay);
-		mNewPersonShouldPayInput = (EditText)findViewById(R.id.EnterPays_EditText_ShouldPay);
-		mNewPersonShouldPayInput.setOnFocusChangeListener(editTextFocusListener);
-		mNewPersonShouldPayInput.addTextChangedListener(payTextChangedListener);
 		mReceivesGiftCheckBox = (CheckBox) findViewById(R.id.EnterPays_gotGift_checkbox);
 		mReceivesGiftCheckBox.setOnCheckedChangeListener(receivesGiftChangeListener);
 		mBuysGiftCheckBox = (CheckBox) findViewById(R.id.EnterPays_buysGift_checkbox);
 		mBuysGiftCheckBox.setOnCheckedChangeListener(buysGiftChangeListener);
 		mGiftValueInput = (EditText)findViewById(R.id.EnterPays_EditText_giftValue);
-
-		setHowMuchShouldPayFieldsVisibility();
 	}
 	
 	@Override
 	protected void loadInputDataFromBundle(Bundle extras) {
 		super.loadInputDataFromBundle(extras);
-		setHowMuchShouldPayFieldsVisibility();
 		
 		PersonData pd = (PersonData)extras.getSerializable(Constants.BUNDLE_PERSON_TO_EDIT);
 		if(pd!=null){
 			editPersonData = pd;
 			mNewPersonNameInput.setText(pd.getName());
 			mNewPersonPayInput.setText(String.valueOf(pd.getPayMadeByPerson()));
-			mNewPersonShouldPayInput.setText(String.valueOf(pd.getHowMuchPersonShouldPay()));
 			mReceivesGiftCheckBox.setChecked(pd.receivesGift());
 			mBuysGiftCheckBox.setChecked(pd.getHowMuchIPaidForGift() > 0);
 			mGiftValueInput.setText(pd.getHowMuchIPaidForGift() > 0 ? pd.getHowMuchIPaidForGift() + "" : "");
@@ -93,7 +75,6 @@ public class AddNewPerson extends AddNewPersonBase {
 		HashSet<PersonData> personDataSet = new HashSet<PersonData>();
 		double payDouble = FormatterHelper.readDoubleFromEditText(mNewPersonPayInput);
 		String name = mNewPersonNameInput.getText().toString();
-		double shouldPayDouble = FormatterHelper.readDoubleFromEditText(mNewPersonShouldPayInput);
 		boolean receivesGift = mReceivesGiftCheckBox.isChecked();
 		boolean buysGift = mBuysGiftCheckBox.isChecked();
 		double giftPayment = FormatterHelper.readDoubleFromEditText(mGiftValueInput);
@@ -101,10 +82,11 @@ public class AddNewPerson extends AddNewPersonBase {
 		if(!buysGift)
 			giftPayment = 0;
 
-		if(!InputValidator.inputIsValid(getApplicationContext(), name, payDouble, shouldPayDouble, calc.isEqualPayments(), inputPaysList))
+		if(!InputValidator.inputIsValid(getApplicationContext(), name, payDouble, calc.isEqualPayments(), inputPaysList))
 			throw new BadInputDataException();
 
 		if(calc.isEqualPayments())
+			//FIXME: zamienić na prawdziwe atom pays list
 			personDataSet.add(new PersonData(name, PersonDataUtils.getDefaultAtomPaymentsList(payDouble), emails, receivesGift, giftPayment));
 		else{
 			throw new BadInputDataException("should not reach here!");
@@ -113,44 +95,6 @@ public class AddNewPerson extends AddNewPersonBase {
 		return personDataSet;
 	}
 
-
-	void setHowMuchShouldPayFieldsVisibility() {
-		try{
-			if(!calc.isEqualPayments()){
-				mNewPersonShouldPayInput.setVisibility(View.VISIBLE);
-				mNewPersonShouldPayText.setVisibility(View.VISIBLE);
-			}else{
-				mNewPersonShouldPayInput.setVisibility(View.GONE);
-				mNewPersonShouldPayText.setVisibility(View.GONE);
-			}
-		}catch(NullPointerException e){
-			Log.d(LOG_TAG, "Not yet Initialized");
-		}
-	}
-
-	void updateShouldPayTextFields(double editedPay, double editedShouldPay) {
-		if(!calc.isEqualPayments()){
-			double howMuchLeftToPay = InputValidator.validatePaysConsistency(inputPaysList, calc.isEqualPayments(), editedPay, editedShouldPay);
-
-			if(howMuchLeftToPay > 0){
-				mNewPersonShouldPayText.setText(getResources().getString(R.string.EnterPays_TextView_ShouldPay) +
-						"\n" + getResources().getString(R.string.EnterPays_TextView_ShouldPay_HowMuchLeft_Start) +
-						" " + howMuchLeftToPay +
-						getResources().getString(R.string.EnterPays_TextView_ShouldPay_HowMuchLeft_End));
-			}else if (howMuchLeftToPay <0){
-				mNewPersonShouldPayText.setText(getResources().getString(R.string.EnterPays_TextView_ShouldPay) +
-						"\n" + getResources().getString(R.string.EnterPays_TextView_ShouldPay_HowMuchLeft_TooMuch_Start) +
-						" " + -1 * howMuchLeftToPay + " " +
-						getResources().getString(R.string.EnterPays_TextView_ShouldPay_HowMuchLeft_End));
-			}else {
-				mNewPersonShouldPayText.setText(getResources().getString(R.string.EnterPays_TextView_ShouldPay));
-			}
-
-			mGiftValueInput.setVisibility(View.GONE);
-			mBuysGiftCheckBox.setVisibility(View.GONE);
-			mReceivesGiftCheckBox.setVisibility(View.GONE);
-		}
-	}
 
 	OnClickListener getContactClickListener = new OnClickListener() {
 		public void onClick(View v) {
@@ -183,7 +127,6 @@ public class AddNewPerson extends AddNewPersonBase {
 	OnCheckedChangeListener equalPaysChangeListener = new OnCheckedChangeListener(){
 		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
 			calc.setEqualPayments(isChecked);
-			setHowMuchShouldPayFieldsVisibility();
 		}
 	};
 
@@ -208,17 +151,6 @@ public class AddNewPerson extends AddNewPersonBase {
 		}
 	};
 
-	private TextWatcher payTextChangedListener = new TextWatcher() {
-
-		public void onTextChanged(CharSequence s, int start, int before, int count) {
-			updateShouldPayTextFields(FormatterHelper.readDoubleFromEditText(mNewPersonPayInput),
-					FormatterHelper.readDoubleFromEditText(mNewPersonShouldPayInput));
-		}
-
-		public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-		public void afterTextChanged(Editable s) {}
-	};
-	
 	@Override
 	public void onBackPressed() {
 		if(editPersonData!=null){
