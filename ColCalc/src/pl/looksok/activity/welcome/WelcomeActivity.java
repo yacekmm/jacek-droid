5fd688251a17e2f67c081c508f96aae805ec7942
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-10-29 18:45:29 +0000

    poprawka przewijania list i zajebiaszcza animacja zapisanych rozliczen

--- ColCalc/src/pl/looksok/activity/welcome/WelcomeActivity.java
+++ ColCalc/src/pl/looksok/activity/welcome/WelcomeActivity.java
@@ -10,10 +10,13 @@ import pl.looksok.logic.CalculationLogic;
 import pl.looksok.utils.CalcPersistence;
 import pl.looksok.utils.Constants;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -31,20 +34,22 @@ public class WelcomeActivity extends ColCalcActivity {
 		setContentView(R.layout.welcome);
 		
 		((Button)findViewById(R.id.welcome_mode_potluckParty)).setOnClickListener(modePotluckPartyClickListener);
-		populateStoredCalcsList();
+		ReadStoredCalcs readCalcsTask = new ReadStoredCalcs();
+		readCalcsTask.execute();
+	}
+	
+	@Override
+	protected void onResume() {
+		findViewById(R.id.welcome_savedCalcs_list).setVisibility(View.INVISIBLE);
+		super.onResume();
 	}
 
 	private void populateStoredCalcsList() {
-		storedCalcs = CalcPersistence.readStoredCalculationList(getApplicationContext(), Constants.PERSISTENCE_SAVED_CALCS_FILE);
 		if(storedCalcs!=null){
 			adapter = new StoredCalcsListAdapter(WelcomeActivity.this, R.layout.stored_calcs_list_item, storedCalcs);
-			((ListView)findViewById(R.id.welcome_savedCalcs_list)).setAdapter(adapter);
-			((ListView)findViewById(R.id.welcome_savedCalcs_list)).setSelection(0);
-
-			if(adapter.getItems().size() > 0)
-				findViewById(R.id.welcome_noStoredCalcs).setVisibility(View.GONE);
-			else
-				findViewById(R.id.welcome_noStoredCalcs).setVisibility(View.VISIBLE);
+			ListView listView = (ListView)findViewById(R.id.welcome_savedCalcs_list);
+			listView.setAdapter(adapter);
+			listView.setSelection(0);
 		} else{
 			Log.d(LOG_TAG, "Stored calcs is null. no items were saved until now");
 			findViewById(R.id.welcome_noStoredCalcs).setVisibility(View.VISIBLE);
@@ -85,7 +90,47 @@ public class WelcomeActivity extends ColCalcActivity {
 	@Override
 	protected void handleRemoveConfirm(int dialogType) {
 		CalcPersistence.removeCalculationFromList(getApplicationContext(), Constants.PERSISTENCE_SAVED_CALCS_FILE, calcItemToRemove);
-		populateStoredCalcsList();
+		ReadStoredCalcs readCalcsTask = new ReadStoredCalcs();
+		readCalcsTask.execute();
 		calcItemToRemove = null;
+	}
+
+	private void updateHelperVisibility() {
+		if(adapter.getItems().size() > 0)
+			findViewById(R.id.welcome_noStoredCalcs).setVisibility(View.GONE);
+		else
+			findViewById(R.id.welcome_noStoredCalcs).setVisibility(View.VISIBLE);
+		findViewById(R.id.welcome_progressBar).setVisibility(View.INVISIBLE);
+	}
+	
+	private class ReadStoredCalcs extends AsyncTask<Void, Void, Void>{
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			runOnUiThread(new Runnable() {
+				
+				@Override
+				public void run() {
+					findViewById(R.id.welcome_progressBar).setVisibility(View.VISIBLE);
+				}
+			});
+			storedCalcs = CalcPersistence.readStoredCalculationList(getApplicationContext(), Constants.PERSISTENCE_SAVED_CALCS_FILE);
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			try{
+				populateStoredCalcsList();
+				Animation pushUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.push_up_in_slow);
+		        findViewById(R.id.welcome_savedCalcs_list).startAnimation(pushUp);
+		        findViewById(R.id.welcome_savedCalcs_list).setVisibility(View.VISIBLE);
+		        
+		        updateHelperVisibility();
+			}catch(Exception e){
+				
+			}
+			super.onPostExecute(result);
+		}
 	}
 }
