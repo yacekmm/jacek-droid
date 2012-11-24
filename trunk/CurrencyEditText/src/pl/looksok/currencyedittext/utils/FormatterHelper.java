40cf673dde261bd07fa43a41513892b04576a768
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2012-11-24 13:42:58 +0000

    CurrencyEditText dziala jak nalezy

--- trunk/CurrencyEditText/src/pl/looksok/currencyedittext/utils/FormatterHelper.java
+++ trunk/CurrencyEditText/src/pl/looksok/currencyedittext/utils/FormatterHelper.java
@@ -1,10 +1,15 @@
 package pl.looksok.currencyedittext.utils;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.text.ParseException;
 
+import android.util.Log;
 import android.widget.EditText;
 
 public class FormatterHelper {
+	private static final String TAG = FormatterHelper.class.getSimpleName();
+
 	public static double roundDouble(double value, int decimalPlaces){
 	    BigDecimal bd = new BigDecimal(value);
 	    bd = bd.setScale(decimalPlaces, BigDecimal.ROUND_HALF_UP);
@@ -19,5 +24,22 @@ public class FormatterHelper {
     	if(payString.length() > 0)
     		payDouble = Double.parseDouble(payString);
 		return payDouble;
+	}
+	
+	public static String currencyFormat(double value, int fractionDigits){
+		NumberFormat nf = NumberFormat.getCurrencyInstance();
+		nf.setMaximumFractionDigits(fractionDigits);
+		return(nf.format(value));
+	}
+	
+	public static double decodeValueFromCurrency(String text){
+		try {
+			Number number = NumberFormat.getCurrencyInstance().parse(text);
+			return Double.parseDouble(number.toString());
+		} catch (ParseException e) {
+			Log.e(TAG, "ParseException: " + e.getMessage());
+			Log.e(TAG, "Text: " + text);
+			return Double.parseDouble(text);
+		}
 	}
 }
