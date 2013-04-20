9ada4b205cf1d4c8d0d7200512430aff0f176579
jacek.milewski.k@gmail.com <jacek.milewski.k@gmail.com@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2013-04-20 14:09:33 +0000

    init commit

---
+++ trunk/ListViewDemo/src/pl/looksok/listviewdemo/AddNewPerson.java
@@ -0,0 +1,45 @@
+package pl.looksok.listviewdemo;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ListView;
+
+public class AddNewPerson extends Activity {
+
+	private AtomPayListAdapter adapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_view);
+		
+		setupListViewAdapter();
+		
+		setupAddPaymentButton();
+	}
+
+	public void removeAtomPayOnClickHandler(View v) {
+		AtomPayment itemToRemove = (AtomPayment)v.getTag();
+		adapter.remove(itemToRemove);
+	}
+
+	private void setupListViewAdapter() {
+		adapter = new AtomPayListAdapter(AddNewPerson.this, R.layout.atom_pay_list_item, new ArrayList<AtomPayment>());
+		ListView atomPaysListView = (ListView)findViewById(R.id.EnterPays_atomPaysList);
+		atomPaysListView.setAdapter(adapter);
+	}
+
+	private void setupAddPaymentButton() {
+		findViewById(R.id.EnterPays_addAtomPayment).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				adapter.insert(new AtomPayment("", 0), 0);
+			}
+		});
+	}
+}
