89bc4fe6f89f551793958b5822a6d686145df47d
jacek.milewski.k <jacek.milewski.k@65b340c3-4026-5b88-2b5f-1caeb108e90d>
2013-11-02 17:02:11 +0000

    FragmentLifecycle tutorial

--- ViewPagerDemo/src/pl/looksok/viewpagerdemo/FragmentGreen.java
+++ ViewPagerDemo/src/pl/looksok/viewpagerdemo/FragmentGreen.java
@@ -1,12 +1,16 @@
 package pl.looksok.viewpagerdemo;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import android.support.v4.app.Fragment;
 
-public class FragmentGreen extends Fragment {
+public class FragmentGreen extends Fragment implements FragmentLifecycle{
+
+	private static final String TAG = FragmentGreen.class.getSimpleName();
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -14,4 +18,17 @@ public class FragmentGreen extends Fragment {
 		View view = inflater.inflate(R.layout.fragment_green, container, false);
 		return view;
 	}
+	
+	@Override
+	public void onPauseFragment() {
+		Log.i(TAG, "onPauseFragment()");
+		Toast.makeText(getActivity(), "onPauseFragment():" + TAG, Toast.LENGTH_SHORT).show(); 
+	}
+	
+	@Override
+	public void onResumeFragment() {
+		Log.i(TAG, "onResumeFragment()");
+		Toast.makeText(getActivity(), "onResumeFragment():" + TAG, Toast.LENGTH_SHORT).show(); 
+	}
+
 }
--- ViewPagerDemo/src/pl/looksok/viewpagerdemo/MainActivity.java
+++ ViewPagerDemo/src/pl/looksok/viewpagerdemo/MainActivity.java
@@ -3,17 +3,44 @@ package pl.looksok.viewpagerdemo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 
 public class MainActivity extends FragmentActivity {
+
+	private MyPagerAdapter pageAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		MyPagerAdapter pageAdapter = new MyPagerAdapter(getSupportFragmentManager());
+		pageAdapter = new MyPagerAdapter(getSupportFragmentManager());
 		ViewPager pager = (ViewPager)findViewById(R.id.myViewPager);
 		pager.setAdapter(pageAdapter);
+		
+		pager.setOnPageChangeListener(pageChangeListener);
 	}
+	
+	private OnPageChangeListener pageChangeListener = new OnPageChangeListener() {
+		
+		int currentPosition = 0;
+		
+		@Override
+		public void onPageSelected(int newPosition) {
+			
+			FragmentLifecycle fragmentToHide = (FragmentLifecycle)pageAdapter.getItem(currentPosition);
+			fragmentToHide.onPauseFragment();
+
+			FragmentLifecycle fragmentToShow = (FragmentLifecycle)pageAdapter.getItem(newPosition);
+			fragmentToShow.onResumeFragment();
+			
+			currentPosition = newPosition;
+		}
+		
+		@Override
+		public void onPageScrolled(int arg0, float arg1, int arg2) { }
+		
+		public void onPageScrollStateChanged(int arg0) { }
+	};
 
 }
